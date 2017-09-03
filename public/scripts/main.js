476d4541cde2764270c955f486428772ba13c303
Brett Miller <miller.brett.andrew@gmail.com>
2017-09-03 11:20:51 -0500

    added aos

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,4 +1,9 @@
-$(document).ready(function() {
+$(document).scroll(function() {
   console.log('JQ');
+  AOS.init({
+    // startEvent: 'scroll',
+    duration: 1300,
+  });
 
 }); //end on ready
+console.log('JS');
